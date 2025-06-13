007e15385de2da6d49cf34c417f66b66364a582d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-06-13 09:33:58 +0500

    Update footer links to point to the creator's portfolio and GitHub source code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
     <footer className="text-center text-[15px] font-semibold text-gray-900">
 
       <div className="flex flex-col md:flex-row md:gap-5 gap-0 md:mb-0 mb-3">
-        <a href="https://wk-devportfolio.vercel.app/" className="hover:text-blue-900">Explore the <span className="text-blue-900">Portfolio</span> of the website's <span className="text-blue-900">Creator's</span> .</a>
-        <a href="#" className="hover:text-blue-900">View the source code of this <span className="text-blue-900">Chatbot</span> website.</a>
+        <a href="https://wajahat-kamal-portfolio.vercel.app/" target="_blank" className="hover:text-blue-900">Explore the <span className="text-blue-900">Portfolio</span> of the Chatbot's <span className="text-blue-900">Creator</span>.</a>
+        <a href="https://github.com/wajahat-kamal/WK-Chatbot.git" target="_blank" className="hover:text-blue-900">View the source code of this <span className="text-blue-900">Chatbot</span> website.</a>
         </div>
 
         <div className="flex flex-col md:flex-row md:gap-5 gap-0">
