b5fb891accd57f25dafd4221841a6b660f3a270d
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-07-07 18:06:15 +0500

    Add link to GitHub source code in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,6 +9,7 @@ function Footer() {
 
       <div className="flex flex-col md:flex-row md:gap-5 gap-0 md:mb-0 mb-3">
         <a href="https://wajahatkamal-dev.vercel.app/" target="_blank" className="hover:text-blue-900">Explore the <span className="text-blue-900">Portfolio</span> of the Chatbot's <span className="text-blue-900">Creator</span>.</a>
+        <a href="https://github.com/wajahat-kamal/WK-Chatbot.git" target="_blank" className="hover:text-blue-900">View the source code of this <span className="text-blue-900">Chatbot</span> website.</a>
         </div>
 
         <div className="flex flex-col md:flex-row md:gap-5 gap-0">
