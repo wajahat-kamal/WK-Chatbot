be90a3a551a4c333041a14c7875104b628ec635f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-19 10:08:04 +0500

    Refactor Chatbot component: replace ChatbotUI with a new layout, integrate framer-motion for animations, and add lucide-react icons for enhanced functionality. Update index.html to remove unnecessary classes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import ChatBot from "./components/Chatbot";
 
 export default function() {
   return(
-    <div className="h-screen w-full flex flex-col items-center justify-center pb-5">
-       <ChatBot/>
-    </div>
+     <ChatBot/>
   )
 }
--- src/components/ChatbotUI.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ChatbotUI() {
-  return (
-    <div>Chatbot-ui</div>
-  )
-}
-
-export default ChatbotUI
